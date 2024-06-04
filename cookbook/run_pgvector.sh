aa349e316b19ad31c60428820efe9fb4995de276
Aftar Ahmad Sami <aftarahmadsami@gmail.com>
2024-06-04 19:14:10 +0600

    update

--- cookbook/run_pgvector.sh
+++ cookbook/run_pgvector.sh
@@ -1,16 +1,9 @@
-#!/bin/bash
-
-# Run PostgreSQL with pgvector extension in Docker
 docker run -d \
-    -e POSTGRES_DB=defaultdb \
-    -e POSTGRES_USER=avnadmin \
-    -e POSTGRES_PASSWORD=AVNS_R6RE-o-OUS9CapOrd1u \
-    -e PGDATA=/var/lib/postgresql/data/pgdata \
-    -e SERVICE_URL="postgres://avnadmin:AVNS_R6RE-o-OUS9CapOrd1u@pg-338e7d49-sjinnovation.f.aivencloud.com:21557/defaultdb?sslmode=require"\
-    -v pgvolume:/var/lib/postgresql/data\
-    -p 5532:5432\
-    --name pgvector\
-    phidata/pgvector:16
-
-echo "PostgreSQL with pgvector is running."
-echo "Service URL is postgresql://avnadmin:AVNS_R6RE-o-OUS9CapOrd1u@localhost:5532/defaultdb"
+  -e POSTGRES_DB=ai \
+  -e POSTGRES_USER=ai \
+  -e POSTGRES_PASSWORD=ai \
+  -e PGDATA=/var/lib/postgresql/data/pgdata \
+  -v pgvolume:/var/lib/postgresql/data \
+  -p 5532:5432 \
+  --name pgvector \
+  phidata/pgvector:16
